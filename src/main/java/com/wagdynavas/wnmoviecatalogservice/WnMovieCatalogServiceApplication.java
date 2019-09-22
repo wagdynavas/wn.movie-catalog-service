194697cf6dc06fcdcd1d4a5539ca7c5f58277d3a
Dinho <dnavas@sdd.com.ar>
2019-09-22 12:00:02 -0300

    first wn.movie-catalog-service

---
+++ src/main/java/com/wagdynavas/wnmoviecatalogservice/WnMovieCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wagdynavas.wnmoviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnMovieCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnMovieCatalogServiceApplication.class, args);
+    }
+
+}
